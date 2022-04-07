2e86fe83be510b615e46e2ba9471dd95a0a12bcc
Odis Clemons <odis.clemons@gmail.com>
2022-04-06 20:59:47 -0400

    got the fadeout and fade in working for starting the game

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,21 @@
+var qaSection = $("#qa-section");
+var qaStart = $("#qa-start");
+var qaWelcome = $("#qa-welcome");
 var count = 3;
 
 $(() => {
-  console.log("ready");
-  let qaSection = $("#qa-section");
-  //qaSection.fadeIn(1000);
+  qaStart.click(() => {
+    qaWelcome.fadeOut(2000, () => startGame());
+    // startGame();
+  });
+});
 
+function timesUp() {
+  console.log("Time is up!");
+}
+
+function startGame() {
+  qaSection.fadeIn();
   var countDown = setInterval(() => {
     if (count < 0) {
       countDown = null;
@@ -16,8 +27,4 @@ $(() => {
     $("#cur-time").text(count);
     count--;
   }, 1000);
-});
-
-function timesUp() {
-  console.log("Time is up!");
 }
