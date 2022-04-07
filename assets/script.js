247d615fc6888062fe43a263338f2729bd9f69bd
Odis Clemons <odis.clemons@gmail.com>
2022-04-06 20:28:01 -0400

    got timer working

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
-$().ready(() => {
+var count = 60;
+
+$(() => {
   console.log("ready");
+  let qaSection = $("#qa-section");
+  qaSection.fadeIn(10000);
+
+  var countDown = setInterval(() => {
+    if (count === 0) {
+      countDown = null;
+      return;
+    }
+    count--;
+    console.log(count);
+    $("#cur-time").text(count);
+  }, 1000);
 });
